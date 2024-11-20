fc339373a71c3da6dcb57d41c1fee1fee7a77741
Jisoo.R <jisuryu0527@gmail.com>
2024-11-20 22:53:54 +0900

    [Refactor] 코드 및 클래스 파일 정리

--- src/main/java/org/example/expert/ExpertApplication.java
+++ src/main/java/org/example/expert/ExpertApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.example.expert")
 @EnableAspectJAutoProxy
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class ExpertApplication {
